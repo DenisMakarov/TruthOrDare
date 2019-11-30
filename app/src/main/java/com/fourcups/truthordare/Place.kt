92c456b1c266ae3d1cd725e1efc3915b7fa3d249
Pocoder <44253480+DenisMakarov@users.noreply.github.com>
2019-11-30 03:40:00 +0300

    add 3 activities and nonworking buttons

---
+++ app/src/main/java/com/fourcups/truthordare/Place.kt
@@ -0,0 +1,12 @@
+package com.fourcups.truthordare
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Place : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_place)
+    }
+}
---
+++ app/src/main/java/com/fourcups/truthordare/ageRate.kt
@@ -0,0 +1,12 @@
+package com.fourcups.truthordare
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ageRate : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_age_rate)
+    }
+}
